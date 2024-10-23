88ad31d0ae521fca04161be82f79abca80663d9b
Lê Trung Hiếu <hieuck@users.noreply.github.com>
2024-10-23 20:01:50 +0700

    Update popup.js

--- js/popup.js
+++ js/popup.js
@@ -11,19 +11,19 @@ document.addEventListener("DOMContentLoaded", () => {
     chrome.storage.local.get(["selectedLanguage", "customLanguage", "customPrompt", "customLink"], (data) => {
         if (data.selectedLanguage) {
             languageDropdown.value = data.selectedLanguage;
-            statusMessage.innerText += `Ngôn ngữ đã chọn: ${data.selectedLanguage}\n`;
+            statusMessage.innerText += `\nNgôn ngữ đã chọn: ${data.selectedLanguage}\n`;
         }
         if (data.customLanguage) {
             customLanguageInput.value = data.customLanguage;
-            statusMessage.innerText += `Ngôn ngữ tùy chỉnh: ${data.customLanguage}\n`;
+            statusMessage.innerText += `\nNgôn ngữ tùy chỉnh: ${data.customLanguage}\n`;
         }
         if (data.customPrompt) {
             customPromptInput.value = data.customPrompt;
-            statusMessage.innerText += `Nội dung tùy chỉnh: ${data.customPrompt}\n`;
+            statusMessage.innerText += `\nNội dung tùy chỉnh: ${data.customPrompt}\n`;
         }
         if (data.customLink) {
             customLinkInput.value = data.customLink;
-            statusMessage.innerText += `Liên kết trả lời (URL): ${data.customLink}\n`;
+            statusMessage.innerText += `\nLiên kết trả lời (URL): ${data.customLink}\n`;
         }
     });
 
@@ -37,7 +37,7 @@ document.addEventListener("DOMContentLoaded", () => {
         // Lưu ngôn ngữ, nội dung tùy chỉnh và custom link vào chrome.storage
         chrome.storage.local.set({ selectedLanguage, customLanguage, customPrompt, customLink }, () => {
             alert("Cài đặt đã được lưu!");
-            statusMessage.innerText = `Ngôn ngữ đã lưu: ${selectedLanguage}\nNội dung tùy chỉnh: ${customPrompt}\nLiên kết trả lời (URL): ${customLink}`;
+            statusMessage.innerText = `\nNgôn ngữ đã lưu: ${selectedLanguage}\n\nNội dung tùy chỉnh: ${customPrompt}\n\nLiên kết trả lời (URL): ${customLink}`;
 
             // Gửi thông báo đến background để cập nhật menu
             chrome.runtime.sendMessage({ action: "createContextMenus" });
